17e584a193116229f62fe4819bd56c08c08d0ef7
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-04-22 10:26:12 +0200

    Created PacMan class

--- pacman/src/main/java/com/example/PacMan.java
+++ pacman/src/main/java/com/example/PacMan.java
@@ -1,34 +1,8 @@
 package com.example;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
-
-
-/**
- * JavaFX App
- */
-public class PacMan extends Application {
-
-    @Override
-    public void start(Stage stage) {
-
-        Label label = new Label("Hello, world!");
-
-        Scene scene = new Scene(label,500,800);
-
-        // Controller handeling the key presses
-        Controller controller = new Controller();
-        scene.setOnKeyPressed(event -> controller.handleKeyPress(event));
-
-
-        stage.setScene(scene);
-        stage.show();
+public class PacMan {
+    
+    public PacMan() {
+        
     }
-
-    public static void main(String[] args) {
-        launch();
-    }
-
 }
