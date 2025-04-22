256062ccda3ce24cd2271a7f47fb5e180de3d50e
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-04-22 10:37:35 +0200

    Added movement methods to pacman class

--- pacman/src/main/java/com/example/PacMan.java
+++ pacman/src/main/java/com/example/PacMan.java
@@ -2,7 +2,45 @@ package com.example;
 
 public class PacMan {
     
-    public PacMan() {
-        
+    float x;
+    float y;
+    float dx;
+    float dy;
+
+    /**
+     * Creates a new PacMan moving to the right.
+     * @param x
+     * @param y
+     */
+    public PacMan(float x, float y) {
+        this.x = x;
+        this.y = y;
+        this.dx = 1;
+        this.dy = 0;
+    }
+
+    public void move() {
+        this.x += this.dx;
+        this.y += this.dy;
+    }
+
+    public void left() {
+        this.dx = -1;
+        this.dy = 0;
+    }
+
+    public void right() {
+        this.dx = 1;
+        this.dy = 0;
+    }
+
+    public void up() {
+        this.dx = 0;
+        this.dy = -1;
+    }
+
+    public void down() {
+        this.dx = 0;
+        this.dy = 1;
     }
 }
