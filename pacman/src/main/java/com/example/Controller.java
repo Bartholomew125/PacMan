13ea0d6d482ded2cba8e4d0aa3abf5c2fbdb14b8
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-04-22 10:30:32 +0200

    Made controller call movement methods of pacman

--- pacman/src/main/java/com/example/Controller.java
+++ pacman/src/main/java/com/example/Controller.java
@@ -1,12 +1,30 @@
 package com.example;
 
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
 public class Controller {
     
-    public Controller() {}
+    PacMan pacman;
+
+    public Controller(PacMan pacman) {
+        this.pacman = pacman;
+    }
 
     public void handleKeyPress(KeyEvent event) {
+        KeyCode key = event.getCode();
 
+        if (key == KeyCode.LEFT) {
+            this.pacman.left();
+        }
+        if (key == KeyCode.RIGHT) {
+            this.pacman.right();
+        }
+        if (key == KeyCode.UP) {
+            this.pacman.up();
+        }
+        if (key == KeyCode.DOWN) {
+            this.pacman.down();
+        }
     }
 }
