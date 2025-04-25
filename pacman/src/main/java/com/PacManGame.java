96e46e73230b0d8c1cab4c8b2c2f6b4a6c76778e
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-04-25 12:53:48 +0200

    Added main game loop

--- pacman/src/main/java/com/PacManGame.java
+++ pacman/src/main/java/com/PacManGame.java
@@ -1,5 +1,6 @@
 package com;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -21,16 +22,26 @@ public class PacManGame extends Application {
         this.imageView = new ImageView();
         Group root = new Group(imageView);
         Scene scene = new Scene(root,500,600);
+
+        final int fps = 2;
+
+        new AnimationTimer()
+        {
+            long t0 = System.nanoTime();
+
+            public void handle(long t1)
+            {
+                if ((t1 - t0) >= Math.pow(10, 9)/fps) {
+                    t0 = t1;
+                }
+            }
+        }.start();
+        stage.show();
+
+
         stage.setScene(scene);
         stage.show();
 
-        System.out.println("hej1");
-
-    }
-
-    public void update() {
-        Image image = new Image("file:src/main/resources/com/pacman.png");
-        this.imageView.setImage(image);
     }
 
     public static void main(String[] args) {
