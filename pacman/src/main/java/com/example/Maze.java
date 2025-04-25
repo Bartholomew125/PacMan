611477faaca38219dc5cd7f33cfcd20557a2415b
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-04-25 15:30:30 +0200

    Created locateCharaters, Created locatePacman
    Made a general method for locating characters in the maze (String[][]),
    and then made a method which specifically locates the pacman in the maze
    using this method. This is used in the constructor when making the
    pacman, give him his position in the maze.

--- pacman/src/main/java/com/example/Maze.java
+++ pacman/src/main/java/com/example/Maze.java
@@ -3,6 +3,7 @@ package com.example;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.nio.file.Path;
 
 public class Maze {
@@ -14,7 +15,8 @@ public class Maze {
 
     public Maze() {
         this.maze = this.readMazeFromFile();
-        this.pacman = new PacMan();
+        Pos2D pos = this.locatePacman();
+        this.pacman = new PacMan(pos.getX(), pos.getY());
     }
 
     private String[][] readMazeFromFile() {
@@ -31,6 +33,40 @@ public class Maze {
             e.printStackTrace();
             return new String[0][0];
         }
+    }
+
+    /**
+     * Locates the first pacman in the maze
+     * 
+     * @return Position of pacman
+     */
+    private Pos2D locatePacman() {
+        return this.locateCharacter("p")[0];
+    }
+
+    /**
+     * Locates all instances of a character c in the maze and returns them as an
+     * array of 2D positions
+     * 
+     * @return Array of 2D positions of c in the maze
+     */
+    private Pos2D[] locateCharacter(String c) {
+        // Starting list with variable length
+        ArrayList<Pos2D> positions = new ArrayList<Pos2D>();
+
+        for (int y = 0; y < this.maze.length; y++) {
+            for (int x = 0; x < this.maze[y].length; x++) {
+                if (this.maze[y][x] == c) {
+                    positions.add(new Pos2D(x, y));
+                }
+            }
+        }
+
+        return positions.toArray(new Pos2D[positions.size()]);
+    }
+
+    public String[][] getMaze() {
+        return this.maze;
     }
 
     public void update() {
