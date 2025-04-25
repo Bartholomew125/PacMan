bf4324c29e8d1c192440567476d3e45db452a3b4
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-04-25 15:20:13 +0200

    Created a simple class for representing a 2D positions

---
+++ pacman/src/main/java/com/example/Pos2D.java
@@ -0,0 +1,32 @@
+package com.example;
+
+/**
+ * A simple class for representing a 2D position
+ */
+public class Pos2D {
+
+    int x;
+    int y;
+
+    public Pos2D(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+}
