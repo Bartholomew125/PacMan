62f459b5cc08b83055dc68bea1bb932c964da76f
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-05-01 11:29:13 +0200

    Created the AnimatedImage class

---
+++ pacman/src/main/java/com/example/AnimatedImage.java
@@ -0,0 +1,44 @@
+package com.example;
+
+import javafx.scene.image.Image;
+
+public class AnimatedImage {
+    
+    private Image[] frames;
+    private double duration;
+    private int squareSize;
+
+
+    public AnimatedImage(double nanoTimePerFrame, int squareSize) {
+        this.duration = nanoTimePerFrame;
+        this.squareSize = squareSize;
+    }
+
+    /**
+     * Sets the frames of the animated image given the filename prefix, the folder where the files exist, and the number of files to be loaded.
+     * <p>
+     * Example use: 
+     * <pre> 
+     * AnimTim.setFrames("pacmanImages", "pacman", "10"); 
+     * </pre>
+     * @param folder
+     * @param fileName
+     * @param files
+     */
+    public void setFrames(String folder, String fileName, int files) {
+        this.frames = new Image[files];
+        for (int i = 0; i < files; i++) {
+            this.frames[i] = new Image("file:src/main/resources/com/example/"+folder+"/"+fileName+i+".png", 
+                squareSize, squareSize, false, false);
+        }
+    }
+
+    /**
+     * @param time
+     * @return The frame for a given time
+     */
+    public Image getFrame(double time) {
+        int index = (int) ((time % (this.frames.length * this.duration)) / this.duration);
+        return this.frames[index];
+    }
+}
