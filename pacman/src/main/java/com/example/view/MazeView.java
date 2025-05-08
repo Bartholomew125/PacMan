eb60f0b421876496220188c399bb521431e4ba72
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-05-08 14:41:20 +0200

    Created a mazeview class
    This classes purpose it to render the maze, which only includes its
    walls

---
+++ pacman/src/main/java/com/example/view/MazeView.java
@@ -0,0 +1,30 @@
+package com.example.view;
+
+import com.example.model.Maze;
+import com.example.model.Wall;
+
+import javafx.scene.image.Image;
+
+public class MazeView extends AbstractView{
+    
+    private Maze maze;
+    private Image wallImage;
+
+    public MazeView(int width, int height, double positionScaler, Maze maze) {
+        super(width, height, positionScaler);
+
+        this.maze = maze;
+
+        // Load images
+        this.wallImage = new Image("file:src/main/resources/com/example/wall.png", this.getPositionScaler(), this.getPositionScaler(), false, false);
+    }
+
+    @Override
+    public void render(double nanoTime) {
+        this.clear();
+        // Add the walls to the surface
+        for (Wall wall : this.maze.getWalls()) {
+            this.addImageToSurface(wallImage, wall.getX(), wall.getY(), 0);
+        }
+    }
+}
