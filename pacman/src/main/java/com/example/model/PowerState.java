f661f86393fe9e5808b5a029c800e1050ac691b7
aske-wengel-frost <askefrost26@gmail.com>
2025-05-23 14:35:41 +0200

    Added movementmultiplier for pacman.

--- pacman/src/main/java/com/example/model/PowerState.java
+++ pacman/src/main/java/com/example/model/PowerState.java
@@ -9,6 +9,6 @@ public class PowerState extends State {
      * Creates powerstate where Ghosts are edible and and afraid, whilst PacMan can't be eaten.
      * */
     public PowerState() {
-        super(true, 0.04, true, false);
+        super(true, 0.04, true, false, 0.06);
     }
 }
