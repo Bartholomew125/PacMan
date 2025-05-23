640a20995e31fb4b89c1c666bea3ed33b91451d3
aske-wengel-frost <askefrost26@gmail.com>
2025-05-23 14:35:29 +0200

    Added movementMultiplier for pacman.

--- pacman/src/main/java/com/example/model/NormalState.java
+++ pacman/src/main/java/com/example/model/NormalState.java
@@ -9,6 +9,6 @@ public class NormalState extends State {
      * Creates a Normalstate ensuring ghosts can't be eaten whilst PacMan can. 
      * */
     public NormalState() {
-        super(false, 0.06, false, true);
+        super(false, 0.06, false, true, 0.06);
     }
 }
