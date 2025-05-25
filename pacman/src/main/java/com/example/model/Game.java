103fc35ebc6f450f2c6bcfd5eb9ba012a41483eb
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-05-25 12:17:57 +0200

    resetPositions now uses Movable.resetPosition()

--- pacman/src/main/java/com/example/model/Game.java
+++ pacman/src/main/java/com/example/model/Game.java
@@ -186,16 +186,10 @@ public class Game {
 
     public void resetPositions() {
         // Reset pacman
-        Pos2D pos = this.maze.locatePacMan();
-        this.pacMan.setX(pos.getX());
-        this.pacMan.setY(pos.getY());
-        this.pacMan.right();
+        this.pacMan.resetPosition();
 
-        // Reset ghosts
-        Pos2D[] ghostPositions = this.maze.locateGhosts();
-        for (int i = 0; i < this.ghosts.size(); i++) {
-            this.ghosts.get(i).setX(ghostPositions[i].getX());
-            this.ghosts.get(i).setY(ghostPositions[i].getY());
+        for (Ghost g : this.ghosts) {
+            g.resetPosition();
         }
     }
     
