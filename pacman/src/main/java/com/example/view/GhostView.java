d6bec2189d7bc481651359f74c6c6b32ba23e99f
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-05-25 12:10:12 +0200

    Added all the colored ghost animations and checks so it renders right

--- pacman/src/main/java/com/example/view/GhostView.java
+++ pacman/src/main/java/com/example/view/GhostView.java
@@ -6,21 +6,36 @@ import java.util.ArrayList;
 import com.example.model.Ghost;
 
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 
 public class GhostView extends AbstractView {
     
     private ArrayList<Ghost> ghosts;
-    private Image ghostImage;
     private Image ghostAfraidImage;
-    private AnimatedImage ghostAnimatedImage;
+    private AnimatedImage ghostGreenAnimatedImage;
+    private AnimatedImage ghostMintAnimatedImage;
+    private AnimatedImage ghostOrangeAnimatedImage;
+    private AnimatedImage ghostPinkAnimatedImage;
 
     public GhostView(int width, int height, double positionScaler, ArrayList<Ghost> ghosts) {
         super(width, height, positionScaler);
         this.ghosts = ghosts;
 
-        // this.ghostImage = new Image("file:src/main/resources/com/example/GreenGhost.png", this.getPositionScaler(), this.getPositionScaler(), false, false);
-        this.ghostAnimatedImage = new AnimatedImage(300000000, (int) this.getPositionScaler());
-        this.ghostAnimatedImage.loadFramesFromDirectory("ghostFramesGreen", "ghost_", 2);
+        // Green Ghost
+        this.ghostGreenAnimatedImage = new AnimatedImage(300000000, (int) this.getPositionScaler());
+        this.ghostGreenAnimatedImage.loadFramesFromDirectory("ghostGreenFrames", "frame", 2);
+
+        // Mint Ghost
+        this.ghostMintAnimatedImage = new AnimatedImage(300000000, (int) this.getPositionScaler());
+        this.ghostMintAnimatedImage.loadFramesFromDirectory("ghostMintFrames", "frame", 2);
+
+        // Green Ghost
+        this.ghostOrangeAnimatedImage = new AnimatedImage(300000000, (int) this.getPositionScaler());
+        this.ghostOrangeAnimatedImage.loadFramesFromDirectory("ghostOrangeFrames", "frame", 2);
+
+        // Green Ghost
+        this.ghostPinkAnimatedImage = new AnimatedImage(300000000, (int) this.getPositionScaler());
+        this.ghostPinkAnimatedImage.loadFramesFromDirectory("ghostPinkFrames", "frame", 2);
 
         this.ghostAfraidImage = new Image("file:src/main/resources/com/example/ghostAfraid.png", this.getPositionScaler(), this.getPositionScaler(), false, false);
     }
@@ -31,10 +46,21 @@ public class GhostView extends AbstractView {
 
         for (Ghost g : this.ghosts) {
             if (g.getIsAfraid()) {
-                this.addImageToSurface(ghostAfraidImage, g.getX(), g.getY(), 0);
+                this.addImageToSurface(ghostAfraidImage, g.getX(), g.getY(), 0, g.getRotation()==0);
             }
             else {
-                this.addImageToSurface(this.ghostAnimatedImage.getFrame(nanoTime), g.getX(), g.getY(), 0);
+                if (g.getColor() == Color.GREEN) {
+                    this.addImageToSurface(this.ghostGreenAnimatedImage.getFrame(nanoTime), g.getX(), g.getY(), 0, g.getRotation()==0);                    
+                }
+                else if (g.getColor() == Color.MINTCREAM) {
+                    this.addImageToSurface(this.ghostMintAnimatedImage.getFrame(nanoTime), g.getX(), g.getY(), 0, g.getRotation()==0);
+                }
+                else if (g.getColor() == Color.ORANGE) {
+                    this.addImageToSurface(this.ghostOrangeAnimatedImage.getFrame(nanoTime), g.getX(), g.getY(), 0, g.getRotation()==0);
+                }
+                else {
+                    this.addImageToSurface(this.ghostPinkAnimatedImage.getFrame(nanoTime), g.getX(), g.getY(), 0, g.getRotation()==0);
+                }
             }
         }
     }
