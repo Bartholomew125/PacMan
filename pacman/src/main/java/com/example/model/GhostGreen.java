c3b52b4c1a802388a42d94a8bde9b9c79038441c
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-05-25 11:51:50 +0200

    Made colored ghost classes

---
+++ pacman/src/main/java/com/example/model/GhostGreen.java
@@ -0,0 +1,10 @@
+package com.example.model;
+
+import javafx.scene.paint.Color;
+
+public class GhostGreen extends Ghost { 
+
+    public GhostGreen(float x, float y){ 
+        super(x, y, 0, 0, 1, Color.GREEN);
+    }      
+}
---
+++ pacman/src/main/java/com/example/model/GhostMint.java
@@ -0,0 +1,14 @@
+package com.example.model;
+
+
+import javafx.scene.paint.Color;
+
+
+public class GhostMint extends Ghost {
+
+    public GhostMint(float x, float y){ 
+        super(x,y,0,-1,0.06,Color.MINTCREAM);
+    } 
+
+    
+}
