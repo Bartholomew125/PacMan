180fa15496fc7f22b7d7411e73bd1a74c18b5297
aske-wengel-frost <askefrost26@gmail.com>
2025-05-26 13:05:28 +0200

    Centered Game over text.

--- pacman/src/main/java/com/example/view/EndView.java
+++ pacman/src/main/java/com/example/view/EndView.java
@@ -7,40 +7,47 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
-public class EndView extends AbstractView{
+public class EndView extends AbstractView {
 
     private Game game;
     private Text scoreText;
     private Button restartButton;
     private boolean restartButtonPressed;
 
-    public EndView (int width, int height, double positionScaler, Game game) {
+    public EndView(int width, int height, double positionScaler, Game game) {
         super(width, height, positionScaler);
-        
+
         this.game = game;
         this.restartButtonPressed = false;
-        
+
         // Create score text
         this.scoreText = new Text();
         this.scoreText.setFill(Color.WHITE);
         this.scoreText.setFont(Font.font("Emulogic", 50));
-        this.scoreText.setText("Game Over\n  Score: "+ this.game.getScore());
+        this.scoreText.setText("Game Over\n  Score: " + this.game.getScore());
 
         this.restartButton = new Button("Restart");
         this.restartButton.setPrefWidth(150);
         this.restartButton.setPrefHeight(50);
         this.restartButton.setFont(Font.font("Emulogic"));
-        this.restartButton.setLayoutX(width/2 - this.restartButton.getPrefWidth()/2);
-        this.restartButton.setLayoutY(height/1.5);
-        
-        
-        this.restartButton.pressedProperty().addListener((observeable, wasPressed, pressed) -> {this.restartButtonPressed = true;});
+        this.restartButton.setLayoutX(width / 2 - this.restartButton.getPrefWidth() / 2);
+        this.restartButton.setLayoutY(height / 1.5);
+
+        this.restartButton.pressedProperty().addListener((observeable, wasPressed, pressed) -> {
+            this.restartButtonPressed = true;
+        });
+
     }
-    
+
     @Override
     public void render(double nanoTime) {
         this.clear();
-        this.addTextToSurface(this.scoreText, width/5, height/2, 0);
+
+        // Calculate where to place "game-over" text.
+        int placeTextX = (int) ((this.width - scoreText.getLayoutBounds().getWidth()) / 2);
+        int placeTextY = (int) ((this.height - scoreText.getLayoutBounds().getHeight()) / 2);
+        this.scoreText.setText("Game Over\n  Score: " + this.game.getScore());
+        this.addTextToSurface(this.scoreText, placeTextX, placeTextY, 0);
         this.getSurface().getChildren().add(this.restartButton);
     }
 
