6c75c994ecc08ae77e4825ed1c3c68d0955c4a16
big-chips <makar24@student.sdu.dk>
2025-05-26 17:23:29 +0200

    Somthing wiht search algoritm

--- pacman/src/main/java/com/example/controller/Controller.java
+++ pacman/src/main/java/com/example/controller/Controller.java
@@ -1,27 +1,18 @@
 package com.example.controller;
 
-import java.lang.foreign.AddressLayout;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.PriorityQueue;
 import java.util.Random;
 
 import com.example.model.Game;
 import com.example.model.Ghost;
-import com.example.model.Maze;
-import com.example.model.Moveable;
 import com.example.model.PacMan;
 import com.example.model.Pill;
-import com.example.model.Pos2D;
 import com.example.model.Wall;
 import com.example.view.Viewer;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
-import javafx.css.Size;
 import javafx.scene.input.KeyEvent;
-import javafx.util.Duration; 
-import java.util.Map;
+import javafx.util.Duration;
 
 /**
  * The controller class which is responsible for controlling the pacMan and
@@ -30,7 +21,7 @@ import java.util.Map;
 public class Controller {
 
     private PacManController pacManController;
-
+    private GhostController ghostController;
     private Viewer viewer;
     private Game game;
     private long previousNanoTime;
@@ -45,7 +36,9 @@ public class Controller {
         // The framerate of the game
         this.nanosecondsPerFrame = Math.pow(10, 9) / fps;
 
-        this.pacManController = new PacManController(this.game.getPacMan(), this.game.getMaze());
+        this.pacManController = new PacManController(this.game.getPacMan(), this.game.getMaze()); 
+
+        this.ghostController = new GhostController(this.game.getOneGhost(), this.game);
     }
 
     /**
@@ -67,6 +60,7 @@ public class Controller {
             this.previousNanoTime = currentNanoTime;
 
             this.pacManController.update(nanoTime);
+            this.ghostController.update(nanoTime);
 
             this.handleCollisions();
 
@@ -87,7 +81,8 @@ public class Controller {
         PacMan pacMan = this.game.getPacMan();
         Wall[] walls = this.game.getMaze().getWalls();
         Pill[] smallPills = this.game.getSmallPillsArray();
-        Pill[] largePills = this.game.getLargePillsArray(); 
+        Pill[] largePills = this.game.getLargePillsArray();  
+        SearchAlgorithm searchAlgorithm = this.game.getSearchAlgorithm();
 
         // Collision with walls
         for (Wall wall : walls) {
@@ -99,7 +94,6 @@ public class Controller {
             for (Ghost g : this.game.getGhosts()) {
                 if (wall.distanceTo(g) < 1) {
                     g.stop();
-                    dummyPath(g);
                     g.move();
                 }
             }
@@ -167,85 +161,7 @@ public class Controller {
         
     } 
 
-    public ArrayList<Pos2D> path(Pos2D current,  ArrayList<Pos2D> laststep){ 
-        ArrayList<Pos2D> totalpath = new ArrayList<Pos2D>(); 
-        for (Pos2D i : laststep) {
-            current = laststep.get(laststep.size()-1);
-            totalpath.add(current);
-        } 
-        return totalpath;
-
-    } 
-
-    public void logic(Ghost g, PacMan goal, int cost){ 
-        goal = this.game.getPacMan();
-        Pos2D realgoal = new Pos2D(Math.round(goal.getX()), Math.round(goal.getY()));
-        Maze maze = this.game.getMaze();
-        ArrayList<Pos2D> camefrom = new ArrayList<Pos2D>();
-
-        ArrayList<Integer> goalScore = new ArrayList<Integer>(); 
-        goalScore.add(0);
-        cost = 0; 
-
-        ArrayList<Integer> fullScore = new ArrayList<Integer>(); 
-        fullScore.add(cost);
-
-        Pos2D curr = new Pos2D (Math.round(g.getX()),Math.round(g.getY())); 
-        PriorityQueue<Pos2D> q = new PriorityQueue<>(); 
-        q.add(curr); 
-        
-        Map<Pos2D, Integer> explored = new HashMap<>();
-        explored.put(curr, 0);
-
-        while (!q.isEmpty()){ 
-            curr = q.poll();
-            if (curr == realgoal){ 
-                path(curr, camefrom);
-            } 
-            q.remove(curr); 
-        }      
-
-        Pos2D calcNeighbours = new Pos2D(curr.getX(),curr.getY()); 
-        getAvailableNeigbours(calcNeighbours);
-        Map<Pos2D, Pos2D> neighbours = new HashMap<>(); 
-        neighbours.put(calcNeighbours, calcNeighbours);
-
-        for (Map.Entry<Pos2D, Pos2D> entry : neighbours.entrySet()) {
-            int newCost = explored.get(new Pos2D(curr.getX(), curr.getY()));
-            Pos2D nextPos = entry.getValue(); 
-            if (nextPos != null && maze.IsWallAt() != true && explored.containsKey(nextPos)){ 
-                explored.put(nextPos, newCost);
-            } 
-
-            double prio = newCost + distanceToGoal(goal); 
-
-            Pos2D neighbourPos = new Pos2D(entry.getKey().getX(), entry.getKey().getY()); 
-            
-            q.add(neighbourPos); 
-            camefrom.add(neighbourPos);
-            
-        }
-
     
 
-    } 
-    
-    public ArrayList<Pos2D> getAvailableNeigbours(Pos2D pos){  
-        ArrayList<Pos2D> neighbourlist = new ArrayList<Pos2D>();
-        Maze maze = this.game.getMaze(); 
-        for (Pos2D elem : neighbourlist) { 
-            if (elem != null && maze.isWallAt(elem.getX(),elem.getY()) != true){ 
-                neighbourlist.add(elem);
-            }
-        }  
-        return neighbourlist;
-    } 
-
-    public double distanceToGoal(PacMan pacman){
-        Ghost[] g = this.game.getGhosts();
-        int x = Math.round(g[g.length].getX()); 
-        int y = Math.round(g[g.length].getY());
-        return Math.sqrt(Math.pow(pacman.getX() - x, 2) + Math.pow(pacman.getY() - y, 2));
-    }
 
 }
