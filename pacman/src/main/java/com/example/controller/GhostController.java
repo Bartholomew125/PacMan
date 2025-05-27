617bda336486ab483a50d825aaac13db6e2dccb7
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-05-27 16:31:10 +0200

    Made ghostcontroller take pacman instead of game

--- pacman/src/main/java/com/example/controller/GhostController.java
+++ pacman/src/main/java/com/example/controller/GhostController.java
@@ -2,26 +2,33 @@ package com.example.controller;
 
 import java.util.ArrayList;
 
-import com.example.controller.searching.BredthFirstSearch;
-import com.example.model.Game;
+import com.example.controller.searching.SearchAlgorithm;
 import com.example.model.Ghost;
+import com.example.model.PacMan;
+import com.example.model.Pos2D;
 
 /**
  * GhostController class for controlling the ghosts ai
  */
 public class GhostController{
     private ArrayList<Ghost> ghosts;  
-    private Game game;
+    private PacMan pacMan;
 
-    public GhostController(ArrayList<Ghost> ghosts, Game game){ 
+    public GhostController(ArrayList<Ghost> ghosts, PacMan pacMan){ 
         this.ghosts = ghosts;
-        this.game = game;
+        this.pacMan = pacMan;
+        
     } 
 
     public void update(long nanoTime){ 
         for (Ghost ghost : this.ghosts) {
             if (ghost.getMoveStack().isEmpty()){ 
-                ghost.setMoveStack(new BredthFirstSearch(game, ghost).getMoveStack()); 
+
+                SearchAlgorithm sa = ghost.getSearchAlgorithm();
+                sa.setStart(new Pos2D((int) ghost.getX(), (int) ghost.getY()));
+                sa.setGoal(new Pos2D((int) this.pacMan.getX(), (int) this.pacMan.getY()));
+                ghost.setMoveStack(sa.getMoveStack()); 
+
                 if (!ghost.getMoveStack().isEmpty()) {
                     ghost.setDX(ghost.getMoveStack().peek().getX()-ghost.getX());
                     ghost.setDY(ghost.getMoveStack().peek().getY()-ghost.getY());
