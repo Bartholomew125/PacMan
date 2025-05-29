3b419139e2a27501595c456bca3ce30a3c9ba421
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-05-29 21:29:41 +0200

    Added javadoc

--- pacman/src/main/java/com/example/controller/SoundController.java
+++ pacman/src/main/java/com/example/controller/SoundController.java
@@ -4,25 +4,31 @@ import java.util.List;
 
 import javafx.scene.media.AudioClip;
 
+/**
+ * SoundController which is responsible for managing and playing audio clips
+ */
 public class SoundController implements Controller {
 
-    private final AudioClip pacManChomp;
-    private final AudioClip pacManDeath;
-    private final AudioClip pacManEatGhost;
+    private final AudioClip chomp;
+    private final AudioClip death;
+    private final AudioClip eatGhost;
     private AudioClip currentClip;
     private final List<AudioClip> priorityList;
     
+    /**
+     * Creates a new SoundController
+     */
     public SoundController() {
-        this.pacManChomp = this.loadClip("/com/example/sound/pacman_chomp.wav");
-        this.pacManDeath = this.loadClip("/com/example/sound/pacman_death.wav");
-        this.pacManEatGhost = this.loadClip("/com/example/sound/pacman_eatghost.wav");
-        this.currentClip = this.pacManChomp;
+        this.chomp = this.loadClip("/com/example/sound/pacman_chomp.wav");
+        this.death = this.loadClip("/com/example/sound/pacman_death.wav");
+        this.eatGhost = this.loadClip("/com/example/sound/pacman_eatghost.wav");
+        this.currentClip = this.chomp;
         this.currentClip.play();
 
         this.priorityList = new ArrayList<>();
-        this.priorityList.add(this.pacManChomp);
-        this.priorityList.add(this.pacManEatGhost);
-        this.priorityList.add(this.pacManDeath);
+        this.priorityList.add(this.chomp);
+        this.priorityList.add(this.eatGhost);
+        this.priorityList.add(this.death);
     }
 
     @Override
@@ -30,18 +36,32 @@ public class SoundController implements Controller {
         this.playChompSound();
     }
 
+    /**
+     * Attempts to play the death sound
+     */
     public void playDeathSound() {
-        this.playSound(this.pacManDeath);
+        this.playSound(this.death);
     }
 
+    /**
+     * Attempts to play the chomp sound
+     */
     public void playChompSound(){
-        this.playSound(this.pacManChomp);
+        this.playSound(this.chomp);
     }
 
+    /**
+     * Attempts to play the ghost eating sound
+     */
     public void playEatGhostSound(){
-        this.playSound(this.pacManEatGhost);
+        this.playSound(this.eatGhost);
     }
 
+    /**
+     * Plays the given audioclip, but only if it has a higher priority than the one already playing, or there is norting else playing.
+     * 
+     * @param audioClip
+     */
     public void playSound(AudioClip audioClip){
         int newClipPriority = this.priorityList.indexOf(audioClip);
         int currentClipPriority = this.priorityList.indexOf(currentClip);
@@ -56,6 +76,11 @@ public class SoundController implements Controller {
         }
     }
 
+    /**
+     * Loads an audioclip from the given path
+     * @param path
+     * @return Audioclip of path
+     */
     private AudioClip loadClip(String path) {
         return new AudioClip(getClass().getResource(path).toExternalForm());
     }
